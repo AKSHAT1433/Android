f763eb433d75937c0c66293fbeab37557a89404f
AKSHAT1433 <73301831+AKSHAT1433@users.noreply.github.com>
2022-06-27 23:35:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/falltest2/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.falltest2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home= new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, 3000);
+    }
+}
